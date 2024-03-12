97ef2ecff4922f245a33c0202b28f1eff9b222d8
Vikas Nagare <vikasnagare05@gmail.com>
2024-03-12 12:28:43 +0530

    added benchmark test cases

--- src/test/java/com/vidnyan/jsonperformace/benchmark/JsonBenchmarkManualTest.java
+++ src/test/java/com/vidnyan/jsonperformace/benchmark/JsonBenchmarkManualTest.java
@@ -1,41 +1,58 @@
 package com.vidnyan.jsonperformace.benchmark;
 
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonBenchmarkManualTest {
+    private  JsonBenchmarkManual benchmark;
+    private  int iterations = 100;
+    private String inputJson = "";
 
-    @AfterEach
-    void tearDown() {
+    @BeforeEach
+    void setUp() {
+        benchmark = new JsonBenchmarkManual();
+        inputJson= "{}";
     }
 
     @Test
-    void benchmarkJackson() {
+    void benchmarkJackson() throws Exception {
+        benchmark.benchmarkJackson("{}", iterations);
     }
 
     @Test
     void benchmarkGson() {
+        benchmark.benchmarkGson("{}", iterations);
     }
 
     @Test
-    void benchmarkJsonSimple() {
+    void benchmarkJsonSimple() throws ParseException {
+        benchmark.benchmarkJsonSimple("{}", iterations);
     }
 
     @Test
     void benchmarkFastJSON() {
+        benchmark.benchmarkFastJSON("{}", iterations);
     }
 
     @Test
     void benchmarkBoon() {
+        //benchmark.benchmarkBoon("{}", 100);
     }
 
     @Test
-    void benchmarkMoshi() {
+    void benchmarkMoshi() throws IOException {
+        benchmark.benchmarkMoshi("{}", iterations);
     }
 
     @Test
-    void benchmarkLoganSquare() {
+    void benchmarkLoganSquare() throws Exception {
+        benchmark.benchmarkLoganSquare("{}", iterations);
     }
 }
