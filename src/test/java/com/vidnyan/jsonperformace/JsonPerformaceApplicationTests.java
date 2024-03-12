2dbf3abc76b7a0315d1a20af2b00ff6558d567a0
Vikas Nagare <vikasnagare05@gmail.com>
2024-03-12 10:09:49 +0530

    added project files

---
+++ src/test/java/com/vidnyan/jsonperformace/JsonPerformaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidnyan.jsonperformace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPerformaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
